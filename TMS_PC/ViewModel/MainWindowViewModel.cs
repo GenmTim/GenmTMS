80b850643cb597d6d5bbf37ba4b6b0bcb48b819f
GenmCai <467779714@qq.com>
2021-01-24 22:24:57 +0800

    添加自定义标题栏

---
+++ TMS_PC/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_PC.ViewModel
+{
+    class MainWindowViewModel
+    {
+        //public RelayCommand ExitCommand { get; private set; }
+
+        //public RelayCommand MinCommand { get; private set; } = new RelayCommand(() =>
+        //{
+        //    WeakReferenceMessenger.Default.Send("", "WindowMinimize");
+        //});
+
+        //public RelayCommand MaxCommand { get; private set; } = new RelayCommand(() =>
+        //{
+        //    WeakReferenceMessenger.Default.Send("", "WindowMaximize");
+        //});
+    }
+}
