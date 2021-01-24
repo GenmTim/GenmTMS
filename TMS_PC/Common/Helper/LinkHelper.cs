b384956304b5b77f19fc7a8b27061888a52fd6d2
GenmCai <467779714@qq.com>
2021-01-24 11:41:25 +0800

    添加登录界面和主界面，修复主界面菜单栏选中动画Bug

---
+++ TMS_PC/Common/Helper/LinkHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_PC.Common.Helper
+{
+    public class LinkHelper
+    {
+        public static void OpenInBrowser(string url)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            }
+        }
+    }
+}
--- TMS_PC/View/LoginView.xaml.cs
+++ TMS_PC/View/LoginView.xaml.cs
@@ -10,18 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TMS_PC.Common.Helper;
 
-namespace TMS_PC
+namespace TMS_PC.View
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// LoginView.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginView : Window
     {
-        public MainWindow()
+        public LoginView()
         {
             InitializeComponent();
         }
