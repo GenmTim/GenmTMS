90e2a392053b680250a063bc6cfedf9c37a4d133
jin2981108790 <2981108790@qq.com>
2021-01-27 16:05:37 +0800

    增加通知页列表视图内容项

---
+++ TMS_PC/Model/Entity/NotificationItemEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_PC.Model.Entity
+{
+	class NotificationItemEntity
+	{
+		private string imgName;
+		private string content;
+		private string title;
+		private string date;
+
+		public NotificationItemEntity(string imgName, string content, string title, string date)
+		{
+			this.imgName = imgName;
+			this.content = content;
+			this.title = title;
+			this.date = date;
+		}
+
+		public string Date { get => date; set => date = value; }
+		public string Title { get => title; set => title = value; }
+		public string Content { get => content; set => content = value; }
+		public string ImgName { get => imgName; set => imgName = value; }
+	}
+}
