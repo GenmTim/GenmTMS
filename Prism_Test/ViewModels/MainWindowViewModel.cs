184aa641d4d987d017c7b8b3be904530c7976805
GenmCai <codelifehmjj@outlook.com>
2021-03-07 19:32:35 +0800

    测试项目部署

--- Prism_Test/ViewModels/MainWindowViewModel.cs
+++ Prism_Test/ViewModels/MainWindowViewModel.cs
@@ -15,26 +15,29 @@ namespace Prism_Test.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
+        public string LogString { get; set; } = "";
+
         public MainWindowViewModel(IRegionManager regionManager)
         {
             this.regionManager = regionManager;
-            this.NaviagationCommand = new DelegateCommand<string>(NavigationPage);
+            LogString = "测试字符串测试字符串测试字符串测试字符串测试字符串测试字符串测试字符串测试字符串测试字符串测试字符串测试字符串测试字符串测试字符串测试字符串测试字符串测试字符串测试字符串";
+            //this.NaviagationCommand = new DelegateCommand<string>(NavigationPage);
         }
 
-        public DelegateCommand<string> NaviagationCommand { get; set; }
+        //public DelegateCommand<string> NaviagationCommand { get; set; }
 
-        public void NavigationPage(string view)
-        {
-            string go_url = Router.Instance[view];
+        //public void NavigationPage(string view)
+        //{
+        //    string go_url = Router.Instance[view];
 
-            string now_url = Router.Instance[nameof(MainWindow)];
-            int next_index = go_url.IndexOf('/', now_url.Length);
-            string next_view = go_url.Substring(0, next_index + 1);
+        //    string now_url = Router.Instance[nameof(MainWindow)];
+        //    int next_index = go_url.IndexOf('/', now_url.Length);
+        //    string next_view = go_url.Substring(0, next_index + 1);
 
-            NavigationParameters param = new NavigationParameters();
-            param.Add("url", go_url);
+        //    NavigationParameters param = new NavigationParameters();
+        //    param.Add("url", go_url);
 
-            this.regionManager.RequestNavigate("ContentRegion", next_view, param);
-        }
+        //    this.regionManager.RequestNavigate("ContentRegion", next_view, param);
+        //}
     }
 }
