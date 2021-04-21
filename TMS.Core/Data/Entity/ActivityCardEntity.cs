5a34b5cbbae11d3e5428c2f342c14a4d817739fc
2981108790@qq.com <2981108790@qq.com>
2021-04-21 17:12:48 +0800

    增加主观评价时间卡片的数据

--- TMS.Core/Data/Entity/ActivityCardEntity.cs
+++ TMS.Core/Data/Entity/ActivityCardEntity.cs
@@ -11,10 +11,12 @@ namespace TMS.Core.Data.Entity
             InnerRectSize = 8;
             CardWidth = 400;
             DateTime = "Default Time";
+            Name = "名字";
             LineSize = 2;
             OuterRectColor = new SolidColorBrush(Color.FromRgb(243, 246, 249));
             InnerRectColor = new SolidColorBrush(Color.FromRgb(88, 176, 237));
             LineColor = new SolidColorBrush(Color.FromRgb(243, 246, 249));
+            Content = "测试";
         }
 
         public int OuterRectSize { get; set; }
@@ -25,11 +27,15 @@ namespace TMS.Core.Data.Entity
 
         public int CardHeight { get; set; }
 
-        public UserControl CardContent { get; set; }
+		public string Name { get; set; }
+
+		public UserControl CardContent { get; set; }
 
         public string DateTime { get; set; }
 
-        public int LineSize { get; set; }
+		public string Content { get; set; }
+
+		public int LineSize { get; set; }
 
         public Brush LineColor { get; set; }
 
