69333d1823940e8d1c109fcab1fc69d8fefe28ec
GenmCai <codelifehmjj@outlook.com>
2021-04-22 14:49:01 +0800

    网页动态表单

--- TMS.DeskTop/ViewModels/WorkPlace/Recruitment/Requirements/Subitem/TalentPoolViewModel.cs
+++ TMS.DeskTop/ViewModels/WorkPlace/Recruitment/Requirements/Subitem/TalentPoolViewModel.cs
@@ -8,6 +8,7 @@ namespace TMS.DeskTop.ViewModels.WorkPlace.Recruitment.Requirements.Subitem
         public string Readme { get; set; }
         public string Tag { get; set; }
         public string Position { get; set; }
+        public string Avatar { get; set; }
     }
 
     class TalentPoolViewModel
@@ -18,9 +19,9 @@ namespace TMS.DeskTop.ViewModels.WorkPlace.Recruitment.Requirements.Subitem
 
         public TalentPoolViewModel()
         {
-            talentList.Add(new Talent { Name = "周乐", Position = "Golang", Tag = "1年·专科·5-9K", Readme = "乐于学习，敢于挑战，懂得交际" });
-            talentList.Add(new Talent { Name = "TomSail", Position = "Golang", Tag = "2年·本科·4-5K", Readme = "善于沟通" });
-            talentList.Add(new Talent { Name = "叶旭峰", Position = "Golang", Tag = "2年·本科·面议", Readme = "本人热爱程序网页开发 喜欢接触新事务 新技术 学习过程中遇到问题会先自己解决 实在解决不了再..." });
+            talentList.Add(new Talent { Name = "周乐", Position = "Golang", Tag = "1年·专科·5-9K", Readme = "乐于学习，敢于挑战，懂得交际", Avatar= "http://47.101.157.194:8081/static/avatar/target6.jpg" });
+            talentList.Add(new Talent { Name = "TomSail", Position = "Golang", Tag = "2年·本科·4-5K", Readme = "善于沟通", Avatar= "http://47.101.157.194:8081/static/avatar/target7.jpg" });
+            talentList.Add(new Talent { Name = "叶旭峰", Position = "Golang", Tag = "2年·本科·面议", Readme = "本人热爱程序网页开发 喜欢接触新事务 新技术 学习过程中遇到问题会先自己解决 实在解决不了再...", Avatar="http://47.101.157.194:8081/static/avatar/target8.jpg" });
         }
     }
 }
